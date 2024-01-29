9004e4a5bec3935b25817d84e955b44f3c4347fa
Alina <alinaamamontova@gmail.com>
2024-01-29 11:18:09 +0300

    Initial commit

---
+++ src/Gof_creating/builder/example_from_internet/Person.java
@@ -0,0 +1,64 @@
+package Gof_creating.builder.example_from_internet;
+
+import java.util.Set;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private int height;
+    private int weight;
+    private Set<Person> parents;
+
+    public static class Builder {
+        private Person newPerson;
+
+        public Builder() {
+            newPerson = new Person();
+        }
+
+        public Builder withName(String name) {
+            newPerson.name = name;
+            return this;
+        }
+
+        public Builder withSurname(String surname) {
+            newPerson.surname = surname;
+            return this;
+        }
+
+        public Builder withAge(int age) {
+            newPerson.age = age;
+            return this;
+        }
+
+        public Builder withHeight(int height) {
+            newPerson.height = height;
+            return this;
+        }
+
+        public Builder withWeight(int weight) {
+            newPerson.weight = weight;
+            return this;
+        }
+
+        public Builder withParents(Set<Person> parents) {
+            newPerson.parents = parents;
+            return this;
+        }
+
+        public Person build() {
+            return newPerson;
+        }
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person.Builder()
+                .withName("Jane")
+                .withSurname("Doe")
+                .withAge(32)
+                .withHeight(165)
+                .withWeight(70)
+                .build();
+    }
+}
